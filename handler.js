53bcaa478627dc431662253b95337b835058bd53
Lucas Marques <lucasmarques73@hotmail.com>
2021-03-31 16:17:20 -0300

    dojo pt 1

---
+++ handler.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+module.exports.createUser = async (event) => {
+  const { body } = event;
+  const { email } = JSON.parse(body);
+
+  const params = {
+    TableName: 'usersTable',
+    Item: {
+      email,
+    },
+  };
+
+  await documentClient.put(params).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
